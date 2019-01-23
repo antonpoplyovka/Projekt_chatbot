3ce1fce08fa2b7b7fb79060197a7f063f58bc80f
antonpoplyovka <antonpoplyovka@gmail.com>
2019-01-24 00:57:02 +0100

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.xml.sax.SAXException;
-
+import org.telegram.telegrambots.TelegramBotsApi;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.*;
 import java.util.Scanner;
 
-public class Main {
+public class Main  {
   public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException, SAXException, ParserConfigurationException {
     System.out.println("Input name of file: ");
     Scanner scanner = new Scanner(System.in);
--- src/dbWriter.java
+++ src/dbWriter.java
@@ -1,14 +1,11 @@
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 
 class dbWriter {
-  private String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+  private String url = "jdbc:oracle:thin:@localhost:1521:db18c";
   private String username = "system";
-  private String password = "system123";
+  private String password = "Aleks53971";
   public Connection connection;
 
   public int id_of_customers = 1;
@@ -21,45 +18,52 @@ class dbWriter {
 
   public void writerCUSTOMERS(ArrayList<String> list) throws SQLException {
 
-    PreparedStatement statement = this.connection.prepareStatement("insert into CUSTOMERS(ID,NAME,SURNAME,AGE) values (?,?,?,?)");
-    statement.setInt(1, this.id_of_customers);
-    statement.setString(2, list.get(0));
-    statement.setString(3, list.get(1));
-    statement.setString(4, list.get(2));
+    PreparedStatement statement = this.connection.prepareStatement("insert into CUSTOMERS(NAME,SURNAME,AGE) values (?,?,?)");
+
+    statement.setString(1, list.get(0));
+    statement.setString(2, list.get(1));
+    statement.setString(3, list.get(2));
     statement.executeUpdate();
 
   }
 
   public void writeCONTACTS(ArrayList<String> list) throws SQLException {
-    PreparedStatement statement = this.connection.prepareStatement("insert into CONTACTS(ID,ID_CUSTOMER,TYPE,CONTACT) values (?,?,?,?)");
+    PreparedStatement statement = this.connection.prepareStatement("insert into CONTACTS(ID_CUSTOMER,TYPE,CONTACT) values (?,?,?)");
+    Statement statement1 = connection.createStatement();
 
+
+
+
+    ResultSet rs = statement1.executeQuery("SELECT max(ID_CUSTOMER) from CUSTOMERS");
+    int LastCustomer = 0;
+    if(rs.next())
+     LastCustomer = rs.getInt(1);
     for (int i = 3; i < list.size(); i++) {
-      statement.setInt(1, this.id_of_contact);
-      this.id_of_contact++;
-      statement.setInt(2, this.id_of_customers);
+
+      statement.setInt(1, LastCustomer);
       String regex = "[\\d\\s]+";
       if (list.get(i).matches(regex) && (list.get(i).length() == 9) || list.get(i).length() == 11) //phone
       {
-        statement.setInt(3, 2);
+        statement.setInt(2, 2);
       } else {
 
         if (list.get(i).equals("jbr"))//jabber
         {
 
-          statement.setInt(3, 3);
+          statement.setInt(2, 3);
         } else {
           boolean itsmail = false;
           for (int j = 0; j < list.get(i).length(); j++) {
             if (list.get(i).charAt(j) == '@') {
-              statement.setInt(3, 1);//mail
+              statement.setInt(2, 1);//mail
               itsmail = true;
             }
           }
           if (itsmail == false)
-            statement.setInt(3, 0);//unknown
+            statement.setInt(2, 0);//unknown
         }
       }
-      statement.setString(4, list.get(i));
+      statement.setString(3, list.get(i));
       statement.executeUpdate();
     }
 
